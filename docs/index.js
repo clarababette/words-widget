17571879ead186c93d13e13a733afd977ca72b07
clarababette <clara_coetzee@zoho.com>
2022-03-15 16:11:32 +0200

    longest words and hide short words

--- docs/index.js
+++ docs/index.js
@@ -4,28 +4,52 @@ document.addEventListener('DOMContentLoaded', () => {
     '.analyse-btn',
     '.output-sentence',
     '.word-count',
+    '#hide-words',
   ];
 
   const elements = selectors.map((selector) =>
     document.querySelector(selector),
   );
 
-  const [inputSentence, analyseBtn, outputSentence, wordCount] = elements;
+  const [inputSentence, analyseBtn, outputSentence, wordCount, hideWords] =
+    elements;
 
   analyseBtn.addEventListener('click', () => {
     let sentence = inputSentence.value;
     if (sentence !== '') {
       sentence = sentence.trim();
       const words = sentence.split(/\s+/);
+      let longestWords = words.reduce((longWords, word) => {
+        word = word.replace(/\W+/g, '');
+        if (!longWords || word.length > longWords[0].length) {
+          return [word];
+        } else if (word.length == longWords[0].length) {
+          return [word, ...longWords];
+        } else {
+          return longWords;
+        }
+      });
       sentence = words.map((word) => {
         const chars = word.replace(/\W+/g, '');
-        if (chars.length > 4) {
+        if (longestWords.includes(chars)) {
+          word = `<mark class="longest">${word}</mark>`;
+        } else if (chars.length > 4) {
           word = `<mark>${word}</mark>`;
         }
-        return word;
+        return `<span>${word}</span>`;
       });
       outputSentence.innerHTML = sentence.join(' ');
       wordCount.innerHTML = `Word count: ${words.length}`;
+    }
+  });
+
+  hideWords.addEventListener('click', () => {
+    let words = outputSentence.children;
+    for (let i = 0; i < words.length; i++) {
+      let word = words[i].innerHTML
+      if (word.length < 5) {
+        words[i].classList.toggle('hidden');
+      }
     }
   });
 });
