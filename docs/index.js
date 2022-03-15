6cfc0a676f7af3fd3e62acef1f8b36e2dbd67a8a
clarababette <clara_coetzee@zoho.com>
2022-03-15 14:19:23 +0200

    no input fixed

--- docs/index.js
+++ docs/index.js
@@ -3,44 +3,29 @@ document.addEventListener('DOMContentLoaded', () => {
     '.input-sentence',
     '.analyse-btn',
     '.output-sentence',
-    '.word-count'
-  ]
+    '.word-count',
+  ];
 
-  const elements = selectors.map((selector) => document.querySelector(selector));
+  const elements = selectors.map((selector) =>
+    document.querySelector(selector),
+  );
 
-  const [
-    inputSentence,
-    analyseBtn,
-    outputSentence,
-    wordCount
-  ] = elements
-
+  const [inputSentence, analyseBtn, outputSentence, wordCount] = elements;
 
   analyseBtn.addEventListener('click', () => {
     let sentence = inputSentence.value;
-    sentence = sentence.trim();
-    const words = sentence.split(/\s+/);
-    sentence = words.map(word => {
-      const chars = word.replace(/\W+/g, '');
-      if (chars.length > 4) {
-        word = `<mark>${word}</mark>`;
-      }
-      return word
-    })
-    outputSentence.innerHTML = sentence.join(' ')
-    wordCount.innerHTML = `Word count: ${words.length}`
-  })
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+    if (sentence !== '') {
+      sentence = sentence.trim();
+      const words = sentence.split(/\s+/);
+      sentence = words.map((word) => {
+        const chars = word.replace(/\W+/g, '');
+        if (chars.length > 4) {
+          word = `<mark>${word}</mark>`;
+        }
+        return word;
+      });
+      outputSentence.innerHTML = sentence.join(' ');
+      wordCount.innerHTML = `Word count: ${words.length}`;
+    }
+  });
+});
