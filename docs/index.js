18340dd79ac1f95cee67b8f251b196c92e772a5c
clarababette <clara_coetzee@zoho.com>
2022-03-16 16:22:49 +0200

    added local storage

--- docs/index.js
+++ docs/index.js
@@ -1,70 +1,85 @@
 import {Analyse} from './words.js';
 document.addEventListener('DOMContentLoaded', () => {
-  const sentences = new Analyse();
+  
+  const sentences = new Analyse(JSON.parse(localStorage.getItem('sentences')));
 
   const selectors = [
     '.input-sentence',
     '.analyse-btn',
     '.new-sentence',
     '.sentence-bank',
-    '.new-sentence .sentence-details'
+    '.new-sentence .sentence-details',
   ];
 
   const elements = selectors.map((selector) =>
-    document.querySelector(selector)
+    document.querySelector(selector),
   );
 
-  const [
-    inputSentence,
-    analyseBtn,
-    newSentence,
-    sentenceBank,
-    newDetails
-  ] = elements;
+  const [inputSentence, analyseBtn, newSentence, sentenceBank, newDetails] =
+    elements;
 
   Handlebars.registerHelper('included', (list, word) => {
     return list.includes(word.replace(/\W+/g, ''));
-});
-  const detailsTemplate = document.querySelector('#sentence-details-template').innerHTML;
+  });
+  const detailsTemplate = document.querySelector(
+    '#sentence-details-template',
+  ).innerHTML;
   const detailsScript = Handlebars.compile(detailsTemplate);
 
-  const sentenceTemplate = document.querySelector('#sentence-template').innerHTML;
+  const sentenceTemplate =
+    document.querySelector('#sentence-template').innerHTML;
   const sentenceScript = Handlebars.compile(sentenceTemplate);
-  
-  // sentences.allSentences.forEach(sentence => {
-  //   sentenceBank.innerHTML += sentenceScript({words: sentence})
-  // })
-  
+  sentenceBank.innerHTML = sentenceScript({sentences: sentences.allSentences})
+
   const hideWords = () => {
-  let words = document.querySelector('.output-sentence').children;
+    let words = document.querySelector('.output-sentence').children;
     for (let i = 0; i < words.length; i++) {
       let word = words[i].innerHTML;
       if (word.length < 5) {
         words[i].classList.toggle('hidden');
       }
     }
-}
+  };
 
   const addToBank = () => {
     sentenceBank.innerHTML = '';
-    sentences.allSentences.forEach(sentence => {
-      sentenceBank.innerHTML += sentenceScript({ words: sentence })
-        })
-  }
+    localStorage.setItem('sentences', JSON.stringify(sentences.allSentences));
+    sentenceBank.innerHTML = sentenceScript({sentences: sentences.allSentences})
+  };
   const analyseSentence = () => {
     let sentence = inputSentence.value;
-    newDetails.innerHTML = detailsScript(sentences.analyse(sentence))
-  }
+    newDetails.innerHTML = detailsScript(sentences.analyse(sentence));
+  };
 
-  newSentence.addEventListener("click", (event) => {
+  newSentence.addEventListener('click', (event) => {
     const target = event.target;
-    if (target.classList.contains("analyse-btn")) {
+    if (target.classList.contains('analyse-btn')) {
       analyseSentence();
-      addToBank();
     }
-    if (target.id == "hide-words") {
-      hideWords()
+    if (target.id == 'hide-words') {
+      hideWords();
     }
+    if (target.id == 'input-sentence') {
+      if (
+        inputSentence.value == sentences.rawInput &&
+        newDetails.innerHTML != ''
+      ) {
+        inputSentence.value = '';
+        sentences.updateList();
+        addToBank();
+        newDetails.innerHTML = '';
+      }
+    }
+  });
+
+  sentenceBank.addEventListener('click', (event) => {
+    const target = event.target;
+    console.log(target.id)
+    // if (target.classList.contains('sentence')) {
+    //   let words = [...target.children]
+    //   words = words.map(child => child.innerHTML);
+    //   target.innerHTML += `<div>${detailsScript(sentences.analyse(words))}</div>`
+    // }
   })
 
 });
--- src/words.js
+++ src/words.js
@@ -1,7 +1,8 @@
 export class Analyse {
-  constructor(sentences = []) {
-    this._sentences = sentences;
+  constructor(sentences) {
+    this._sentences = sentences ? sentences : [];
     this._words;
+    this._rawInput;
   }
   get longestWords() {
      let longestWords = this._words.reduce((longWords, word) => {
@@ -20,23 +21,30 @@ export class Analyse {
     return this._words.filter(word => word.replace(/\W+/g, '').length > 4)
   }
   get average() {
-    console.log(this._sentences.reduce((sum, sentence) => sum + sentence.length, 0) / this._sentences.length)
-    return this._sentences.reduce((sum, sentence) => sum + sentence.length, 0) / this._sentences.length;
+    return this._sentences.reduce((sum, sentence) => sum + sentence.words.length, 0) / this._sentences.length;
   }
   analyse(sentence) {
+    this._rawInput = typeof sentence == "string" ? sentence : '';
     if (sentence !== '') {
-      this._words = sentence.trim().split(/\s+/);
-      this._sentences = this._sentences.length == 5 ? [this._words, ...this._sentences.slice(0, 4)] : [this._words, ...this._sentences]
+      this._words = typeof sentence == "string" ? sentence.trim().split(/\s+/) : sentence;
       return {
         words: this._words,
         longestWords: this.longestWords,
         moreThanFourChar: this.moreThanFourChar,
         wordCount: this._words.length,
-        avg: this._words.length >= this.average ? 'aboveAvg' : 'belowAvg'
+        avg: this._sentences.length == 0 ? '' : this._words.length >= this.average ? 'aboveAvg' : 'belowAvg'
       }
     }
   }
   get allSentences() {
     return this._sentences;
   }
+  get rawInput() {
+    return this._rawInput;
+  }
+  updateList() {
+    if (!this._sentences.includes(this._words)) {
+      this._sentences = this._sentences.length == 5 ? [this._words, ...this._sentences.slice(0, 4)] : [{ words: this._words }, ...this._sentences]
+    }
+    }
 }
