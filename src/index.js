d99b1ee1eef91213b3f06d8f4cf7d7d7ddf9d9ed
clarababette <clara_coetzee@zoho.com>
2022-03-17 11:33:22 +0200

    slider and variable hiding added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import {Analyse} from './words.js';
 document.addEventListener('DOMContentLoaded', () => {
-  
   const sentences = new Analyse(JSON.parse(localStorage.getItem('sentences')));
 
   const selectors = [
@@ -19,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
     elements;
 
   Handlebars.registerHelper('included', (list, word) => {
-    return list.includes(word.replace(/\W+/g, ''));
+    return list.includes(word);
   });
   const detailsTemplate = document.querySelector(
     '#sentence-details-template',
@@ -29,14 +28,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const sentenceTemplate =
     document.querySelector('#sentence-template').innerHTML;
   const sentenceScript = Handlebars.compile(sentenceTemplate);
-  sentenceBank.innerHTML = sentenceScript({sentences: sentences.allSentences})
+  sentenceBank.innerHTML = sentenceScript({sentences: sentences.allSentences});
 
   const hideWords = () => {
     let words = document.querySelector('.output-sentence').children;
+    const hide = sentences.wordsShorter(
+      document.querySelector('#hideLength').value
+    );
+    console.log(document.querySelector('#hideLength').value)
+    console.log(hide)
     for (let i = 0; i < words.length; i++) {
       let word = words[i].innerHTML;
-      if (word.length < 5) {
-        words[i].classList.toggle('hidden');
+      if (hide.includes(word) && !words[i].classList.contains('hidden')) {
+        words[i].classList.add('hidden');
+      } else if (!hide.includes(word) && words[i].classList.contains('hidden')) {
+        words[i].classList.remove('hidden');
       }
     }
   };
@@ -44,7 +50,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const addToBank = () => {
     sentenceBank.innerHTML = '';
     localStorage.setItem('sentences', JSON.stringify(sentences.allSentences));
-    sentenceBank.innerHTML = sentenceScript({sentences: sentences.allSentences})
+    sentenceBank.innerHTML = sentenceScript({
+      sentences: sentences.allSentences,
+    });
   };
   const analyseSentence = () => {
     let sentence = inputSentence.value;
@@ -53,11 +61,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
   newSentence.addEventListener('click', (event) => {
     const target = event.target;
-    if (target.classList.contains('analyse-btn')) {
+    if (
+      target.classList.contains('analyse-btn') &&
+      inputSentence.value !== ''
+    ) {
+      console.log(inputSentence.value);
       analyseSentence();
     }
     if (target.id == 'hide-words') {
-      hideWords();
+      console.log(target.checked)
+      if (target.checked) {
+        hideWords();
+      } else {
+        let words = document.querySelector('.output-sentence').children;
+        for (let i = 0; i < words.length; i++) {
+          if (words[i].classList.contains('hidden')) {
+            words[i].classList.remove('hidden');
+          }
+        }
+      }
     }
     if (target.id == 'input-sentence') {
       if (
@@ -72,14 +94,33 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+  newSentence.addEventListener('change', (event) => {
+    if (event.target.id == 'hideLength') {
+      const length = event.target.value;
+      document.querySelector(
+        '.hideLength',
+      ).innerHTML = `Word limit: ${length} characters`;
+
+      document.querySelector(
+        'label[for=hide-words]',
+      ).innerHTML = `Hide all words fewer than ${length} characters long.`;
+      if (document.querySelector('#hide-words').checked) {
+        hideWords()
+      }
+    }
+  });
+
   sentenceBank.addEventListener('click', (event) => {
     const target = event.target;
-    console.log(target.id)
-    // if (target.classList.contains('sentence')) {
-    //   let words = [...target.children]
-    //   words = words.map(child => child.innerHTML);
-    //   target.innerHTML += `<div>${detailsScript(sentences.analyse(words))}</div>`
-    // }
-  })
-
+    if (
+      inputSentence.value == sentences.rawInput &&
+      newDetails.innerHTML != '' &&
+      inputSentence.value != ''
+    ) {
+      sentences.updateList();
+      addToBank();
+    }
+    inputSentence.value = sentences.allSentences[target.id].words.join(' ');
+    analyseSentence();
+  });
 });
--- src/words.js
+++ src/words.js
@@ -5,35 +5,46 @@ export class Analyse {
     this._rawInput;
   }
   get longestWords() {
-     let longestWords = this._words.reduce((longWords, word) => {
-        word = word.replace(/\W+/g, '');
-        if (!longWords || word.length > longWords[0].length) {
-          return [word];
-        } else if (word.length == longWords[0].length) {
-          return [word, ...longWords];
-        } else {
-          return longWords;
-        }
-     });
-    return longestWords
+    let longestWords = this._words.reduce((longWords, word) => {
+      word = word.replace(/\W+/g, '');
+      if (!longWords || word.length > longWords[0].length) {
+        return [word];
+      } else if (word.length == longWords[0].length) {
+        return [word, ...longWords];
+      } else {
+        return longWords;
+      }
+    });
+    return longestWords;
   }
   get moreThanFourChar() {
-    return this._words.filter(word => word.replace(/\W+/g, '').length > 4)
+    return this._words.filter((word) => word.replace(/\W+/g, '').length > 4);
   }
   get average() {
-    return this._sentences.reduce((sum, sentence) => sum + sentence.words.length, 0) / this._sentences.length;
+    console.log(this._sentences)
+    return (
+      this._sentences.reduce(
+        (sum, sentence) => sum + sentence.words.length,
+        0,
+      ) / this._sentences.length
+    );
   }
   analyse(sentence) {
-    this._rawInput = typeof sentence == "string" ? sentence : '';
-    if (sentence !== '') {
-      this._words = typeof sentence == "string" ? sentence.trim().split(/\s+/) : sentence;
+    this._rawInput = sentence;
+    if (sentence !== '' || sentence !== null) {
+      this._words = sentence.trim().split(/\s+/);
       return {
         words: this._words,
         longestWords: this.longestWords,
         moreThanFourChar: this.moreThanFourChar,
         wordCount: this._words.length,
-        avg: this._sentences.length == 0 ? '' : this._words.length >= this.average ? 'aboveAvg' : 'belowAvg'
-      }
+        avg:
+          this._sentences.length == 0
+            ? ''
+            : this._words.length >= this.average
+            ? 'aboveAvg'
+            : 'belowAvg',
+      };
     }
   }
   get allSentences() {
@@ -44,7 +55,13 @@ export class Analyse {
   }
   updateList() {
     if (!this._sentences.includes(this._words)) {
-      this._sentences = this._sentences.length == 5 ? [this._words, ...this._sentences.slice(0, 4)] : [{ words: this._words }, ...this._sentences]
+      this._sentences =
+        this._sentences.length == 5
+        ? [{ words: this._words }, ...this._sentences.slice(0, 4)]
+          : [{words: this._words}, ...this._sentences];
     }
-    }
+  }
+  wordsShorter(length) {
+    return this._words.filter((word) => word.replace(/\W+/g, '').length < length);
+  }
 }
